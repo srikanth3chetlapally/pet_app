c6350cc38fe980c43ec7fa30e884fa3b8658468b
srikanth3chetlapally <144646023+srikanth3chetlapally@users.noreply.github.com>
2024-09-23 01:06:36 -0400

    forms.py

--- forms.py
+++ forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
 
-class AddPetForm(FlaskForm):
-    name = StringField('Pet Name', validators=[DataRequired(), Length(min=2, max=100)])
-    age = IntegerField('Pet Age', validators=[DataRequired()])
-    pet_type = SelectField('Pet Type', choices=[('Dog', 'Dog'), ('Cat', 'Cat'), ('Bird', 'Bird'), ('Other', 'Other')], validators=[DataRequired()])
-    breed = StringField('Breed (Optional)', validators=[Length(max=100)])
-    submit = SubmitField('Add Pet')
+class PetForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age is', validators=[DataRequired()])
+    type = StringField('Type', validators=[DataRequired()])
+    submit = SubmitField('Add pet to Database')
