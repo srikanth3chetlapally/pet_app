d77aa6951ca8defbe8be960d07166b02dce45a9b
srikanth3chetlapally <144646023+srikanth3chetlapally@users.noreply.github.com>
2024-09-23 01:07:08 -0400

    models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,7 +8,3 @@ class Pet(db.Model):
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     type = db.Column(db.String(100), nullable=False)
-    breed = db.Column(db.String(100), nullable=True)  # Added breed for a different design
-
-    def __repr__(self):
-        return f'<Pet {self.name}>'
